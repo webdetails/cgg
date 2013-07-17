1dfda72f927d36bcae31d245c91f1d776af12366
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-07-17 23:33:20 +0100

    [FIX] (MultiChart Cliping) Small-charts beyond the initial chart size would appear clipped. [FEAT][BREAK] (preExecution) the preExecution handler is now called in CGG as well to avoid normal user misundertsanding.    Attention, that this may break older charts that counted on preExecution not being called in CGG. [FEAT] (parammultiChartOverflow) This parameter can be specified in the url to override the chart's default setting. [FEAT] (CCC MultiChart Overflow) New option multiChartOverflow controls whether to 'grow' (the default value), 'fit' or 'clip' if small charts would otherwise not fit in the initially given chart size.

--- cgg-core/src/pt/webdetails/cgg/resources/ccc-util.js
+++ cgg-core/src/pt/webdetails/cgg/resources/ccc-util.js
@@ -65,67 +65,67 @@ jQuery.isPlainObject = function( obj ) {
 }
 $.support = {};
 $.extend = function() {
-	 var options, name, src, copy, copyIsArray, clone,
-		target = arguments[0] || {},
-		i = 1,
-		length = arguments.length,
-		deep = false;
+     var options, name, src, copy, copyIsArray, clone,
+        target = arguments[0] || {},
+        i = 1,
+        length = arguments.length,
+        deep = false;
 
-	// Handle a deep copy situation
-	if ( typeof target === "boolean" ) {
-		deep = target;
-		target = arguments[1] || {};
-		// skip the boolean and the target
-		i = 2;
-	}
+    // Handle a deep copy situation
+    if ( typeof target === "boolean" ) {
+        deep = target;
+        target = arguments[1] || {};
+        // skip the boolean and the target
+        i = 2;
+    }
 
-	// Handle case when target is a string or something (possible in deep copy)
-	if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
-		target = {};
-	}
+    // Handle case when target is a string or something (possible in deep copy)
+    if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+        target = {};
+    }
 
-	// extend jQuery itself if only one argument is passed
-	if ( length === i ) {
-		target = this;
-		--i;
-	}
+    // extend jQuery itself if only one argument is passed
+    if ( length === i ) {
+        target = this;
+        --i;
+    }
 
-	for ( ; i < length; i++ ) {
-		// Only deal with non-null/undefined values
-		if ( (options = arguments[ i ]) != null ) {
-			// Extend the base object
-			for ( name in options ) {
-				src = target[ name ];
-				copy = options[ name ];
+    for ( ; i < length; i++ ) {
+        // Only deal with non-null/undefined values
+        if ( (options = arguments[ i ]) != null ) {
+            // Extend the base object
+            for ( name in options ) {
+                src = target[ name ];
+                copy = options[ name ];
 
-				// Prevent never-ending loop
-				if ( target === copy ) {
-					continue;
-				}
+                // Prevent never-ending loop
+                if ( target === copy ) {
+                    continue;
+                }
 
-				// Recurse if we're merging plain objects or arrays
-				if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
-					if ( copyIsArray ) {
-						copyIsArray = false;
-						clone = src && jQuery.isArray(src) ? src : [];
+                // Recurse if we're merging plain objects or arrays
+                if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
+                    if ( copyIsArray ) {
+                        copyIsArray = false;
+                        clone = src && jQuery.isArray(src) ? src : [];
 
-					} else {
-						clone = src && jQuery.isPlainObject(src) ? src : {};
-					}
+                    } else {
+                        clone = src && jQuery.isPlainObject(src) ? src : {};
+                    }
 
-					// Never move original objects, clone them
-					target[ name ] = jQuery.extend( deep, clone, copy );
+                    // Never move original objects, clone them
+                    target[ name ] = jQuery.extend( deep, clone, copy );
 
-				// Don't bring in undefined values
-				} else if ( copy !== undefined ) {
-					target[ name ] = copy;
-				}
-			}
-		}
-	}
+                // Don't bring in undefined values
+                } else if ( copy !== undefined ) {
+                    target[ name ] = copy;
+                }
+            }
+        }
+    }
 
-	// Return the modified object
-	return target;
+    // Return the modified object
+    return target;
 };
 
 $.tipsy = function(){};
@@ -140,7 +140,7 @@ function getCccType(type) {
 
 // NOTE: this function must be in sync with that of 
 // BaseCccComponent#_preProcessChartDefinition
-function preProcessChartDefinition(chartDef){
+function preProcessChartDefinition(chartDef) {
     if(chartDef){
         // Obtain effective compatVersion
         var compatVersion = chartDef.compatVersion;
@@ -175,35 +175,93 @@ function preProcessChartDefinition(chartDef){
 }
 
 function renderCccFromComponent(component, data) {
-
-	if(typeof component.postFetch === 'function'){
-		try{
-			data = component.postFetch(data);
-		} catch(e) {
-            // ignore
-            print("Error in postfetch: " + e);
+    try {
+    
+        // Adding ability to process external height and width
+        var cd = component.chartDefinition;
+        var w = cd.width  = +(parseFloat(params.get('width' )) || cd.width );
+        var h = cd.height = +(parseFloat(params.get('height')) || cd.height);
+        
+        var multiChartOverflow = params.get('multiChartOverflow');
+        if(multiChartOverflow) { cd.multiChartOverflow = multiChartOverflow; }
+        
+        var noChartBg = params.get('noChartBg') === 'true';
+        
+        var svgElem = document.documentElement;
+        
+        print("\nCGG PRINT CCC CHART " + component.type + " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+              " Debug:     " + pvc.debug + "\n" + 
+              " Width:     " + w         + "\n" + 
+              " Height:    " + h         + "\n" +
+              " NoChartBg: " + noChartBg + "\n" +
+              " MultiChartOverflow: " + (cd.multiChartOverflow || 'grow'));
+        
+        // Chart Background
+        var bg;
+        if(!noChartBg) {
+            bg = document.createElementNS('http://www.w3.org/2000/svg','rect');
+            bg.setAttribute('id',     'foo_35434765');
+            bg.setAttribute('x',      '0');
+            bg.setAttribute('y',      '0');
+            bg.setAttribute('width',   w);
+            bg.setAttribute('height',  h);
+            bg.setAttribute('style',   'fill:white');
+            svgElem.appendChild(bg);
         }
-	}
-    
-    preProcessChartDefinition(component.chartDefinition);
-    
-    var o = $.extend({}, component.chartDefinition);
-    o.showTooltips = false;
-    o.clickable    = false;
-    o.selectable   = false;
-    o.hoverable    = false;
-    o.tooltipFormat = function(){};
-    
-    var ChartType = getCccType(component.type);
-    var chart = new ChartType(o);
-    
-    chart.setData(data, {
-        crosstabMode: component.crosstabMode,
-        seriesInRows: component.seriesInRows
-    });
-    chart.render();
+        
+        // This may cause problems to older dashboards
+        // that trusted on preExecution not being called in CGG.
+        if(typeof component.preExecution === 'function') {
+            try {
+                var contin = component.preExecution();
+                if(contin != undefined && !contin) {
+                    print("Ignoring 'preExecution' falsy result.");
+                }
+            } catch(ex) {
+                // ignore
+                print("Error in 'preExecution': " + ex);
+            }
+        }
+        
+        if(typeof component.postFetch === 'function') {
+            try {
+                var newData = data = component.postFetch(data);
+                if(newData !== undefined) { data = newData; }
+            } catch(ex) {
+                // ignore
+                print("Error in 'postFetch': " + ex);
+            }
+        }
+        
+        preProcessChartDefinition(component.chartDefinition);
+        
+        cd = $.extend({}, component.chartDefinition);
+        cd.interactive = false;
+        
+        var ChartType = getCccType(component.type);
+        var chart = new ChartType(cd);
+        chart.setData(data);
+        chart.render();
+        
+        // Set the SVG element with the final chart width/height
+        //  so that the PNGTranscoder can detect the image size.
+        var basePanel = chart.basePanel;
+        w = basePanel.width ;
+        h = basePanel.height;
+        
+        svgElem.setAttribute('width',  w);
+        svgElem.setAttribute('height', h);
+        
+        // Also, fix the bg size
+        if(bg) {
+            bg.setAttribute('width',  w);
+            bg.setAttribute('height', h);
+        }
+    } catch(ex) {
+        print("Error while rendering CCC chart: " + ex);
+        throw ex;
+    }
 }
-
 
 var Dashboards = Dashboards || {};
 Dashboards.propertiesArrayToObject = function(pArray) {
@@ -223,9 +281,6 @@ Dashboards.objectToPropertiesArray = function(obj) {
   return pArray;
 };
 
-Dashboards.log = function(m,type){ 
-	if (type)
-		print(type + ": " + m);
-	else
-		print("LOG: " + m);
+Dashboards.log = function(m, type) {
+    print((type || 'LOG').toUpperCase() + ": " + m);
 };
