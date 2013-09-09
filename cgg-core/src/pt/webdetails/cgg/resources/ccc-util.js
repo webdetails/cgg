8d6f6f075743e153cc3967cc372e2abb2c210859
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-09-09 12:38:28 +0100

    [REF] (protovis batik) protovis code no longer has the "batik" string... Method overrides are now being made to cope with different implementations. [FIX] (CCC Render multiple animation) Problems when rendering and re-rendering, possibly with multiple charts to the same canvas, and animations.

--- cgg-core/src/pt/webdetails/cgg/resources/ccc-util.js
+++ cgg-core/src/pt/webdetails/cgg/resources/ccc-util.js
@@ -27,31 +27,31 @@ jQuery.isPlainObject = function(ele){
   for ( key in obj ) {}
   
   return key === undefined || hasOwn.call( obj, key );
-}
+};
 jQuery.isArray = Array.isArray || function( obj ) {
   return jQuery.type(obj) === "array";
-}
+};
 jQuery.isWindow = function( obj ) {
   return obj && typeof obj === "object" && "setInterval" in obj;
-}
+};
 jQuery.type = function( obj ) {
   return obj == null ?
      String( obj ) :
      "object";
-}
+};
 
 jQuery.isPlainObject = function( obj ) {
   // Must be an Object.
   // Because of IE, we also have to check the presence of the constructor property.
-  // Make sure that DOM nodes and window objects don't pass through, as well
-  if ( !obj || jQuery.type(obj) !== "object" || obj.nodeType || jQuery.isWindow( obj ) ) {
+  // Make sure that DOM nodes and window objects don't pass through, as well.
+  if(!obj || jQuery.type(obj) !== "object" || obj.nodeType || jQuery.isWindow(obj)) {
      return false;
   }
   
   // Not own constructor property must be Object
-  if ( obj.constructor &&
+  if(obj.constructor &&
      !hasOwn.call(obj, "constructor") &&
-     !hasOwn.call(obj.constructor.prototype, "isPrototypeOf") ) {
+     !hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
      return false;
   }
   
@@ -59,20 +59,20 @@ jQuery.isPlainObject = function( obj ) {
   // if last one is own, then all properties are own.
 
   var key;
-  for ( key in obj ) {}
-  
-  return key === undefined || hasOwn.call( obj, key );
-}
+  for(key in obj) {}
+  return key === undefined || hasOwn.call(obj, key);
+};
+
 $.support = {};
 $.extend = function() {
-     var options, name, src, copy, copyIsArray, clone,
-        target = arguments[0] || {},
-        i = 1,
-        length = arguments.length,
-        deep = false;
+  var options, name, src, copy, copyIsArray, clone,
+      target = arguments[0] || {},
+      i = 1,
+      length = arguments.length,
+      deep = false;
 
     // Handle a deep copy situation
-    if ( typeof target === "boolean" ) {
+    if(typeof target === "boolean") {
         deep = target;
         target = arguments[1] || {};
         // skip the boolean and the target
@@ -80,45 +80,42 @@ $.extend = function() {
     }
 
     // Handle case when target is a string or something (possible in deep copy)
-    if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+    if(typeof target !== "object" && !jQuery.isFunction(target)) {
         target = {};
     }
 
     // extend jQuery itself if only one argument is passed
-    if ( length === i ) {
+    if(length === i) {
         target = this;
         --i;
     }
 
-    for ( ; i < length; i++ ) {
+    for( ; i < length; i++) {
         // Only deal with non-null/undefined values
-        if ( (options = arguments[ i ]) != null ) {
+        if((options = arguments[i]) != null) {
             // Extend the base object
-            for ( name in options ) {
-                src = target[ name ];
-                copy = options[ name ];
+            for(name in options) {
+                src  = target[name];
+                copy = options[name];
 
                 // Prevent never-ending loop
-                if ( target === copy ) {
-                    continue;
-                }
+                if(target === copy) { continue; }
 
                 // Recurse if we're merging plain objects or arrays
-                if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
-                    if ( copyIsArray ) {
+                if(deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
+                    if(copyIsArray) {
                         copyIsArray = false;
                         clone = src && jQuery.isArray(src) ? src : [];
-
                     } else {
                         clone = src && jQuery.isPlainObject(src) ? src : {};
                     }
 
                     // Never move original objects, clone them
-                    target[ name ] = jQuery.extend( deep, clone, copy );
+                    target[name] = jQuery.extend(deep, clone, copy);
 
                 // Don't bring in undefined values
-                } else if ( copy !== undefined ) {
-                    target[ name ] = copy;
+                } else if(copy !== undefined) {
+                    target[name] = copy;
                 }
             }
         }
@@ -128,11 +125,11 @@ $.extend = function() {
     return target;
 };
 
-$.tipsy = function(){};
-pv.Behavior.tipsy = function(){};
+$.tipsy = function() {};
+pv.Behavior.tipsy = function() {};
 
 function getCccType(type) {
-    if(type){
+    if(type) {
         var className = type.replace(/^ccc(.*)$/, '$1');
         return pvc[className];
     }
@@ -141,28 +138,28 @@ function getCccType(type) {
 // NOTE: this function must be in sync with that of 
 // BaseCccComponent#_preProcessChartDefinition
 function preProcessChartDefinition(chartDef) {
-    if(chartDef){
+    if(chartDef) {
         // Obtain effective compatVersion
         var compatVersion = chartDef.compatVersion;
-        if(compatVersion == null){
+        if(compatVersion == null) {
             compatVersion = typeof pvc.defaultCompatVersion === 'function' ? 
                             pvc.defaultCompatVersion() :
                             1;
         }
         
-        if(compatVersion <= 1){
+        if(compatVersion <= 1) {
             // Properties that are no more registered in the component
             // and that had a name mapping.
             // The default mapping, for unknown properties, doesn't work.
-            if('showLegend' in chartDef){
+            if('showLegend' in chartDef) {
                 chartDef.legend = chartDef.showLegend;
                 delete chartDef.showLegend;
             }
             
             // Don't presume chartDef props must be own
-            for(var p in chartDef){
+            for(var p in chartDef) {
                 var m = /^barLine(.*)$/.exec(p);
-                if(m){
+                if(m) {
                     p2 = 'secondAxis' + (m[1] || '');
                     chartDef[p2] = chartDef[p];
                     delete chartDef[p];
@@ -269,17 +266,21 @@ function renderCccFromComponent(component, data) {
 var Dashboards = Dashboards || {};
 Dashboards.propertiesArrayToObject = function(pArray) {
   var obj = {};
-  for (p in pArray) if (pArray.hasOwnProperty(p)) {
-    var prop = pArray[p];
-    obj[prop[0]] = prop[1];
+  for(var p in pArray) {
+    if(pArray.hasOwnProperty(p)) {
+      var prop = pArray[p];
+      obj[prop[0]] = prop[1];
+    }
   }
   return obj;
 };
 
 Dashboards.objectToPropertiesArray = function(obj) {
   var pArray = [];
-  for (key in obj) if (obj.hasOwnProperty(key)) {
-    pArray.push([key,obj[key]]);
+  for(var key in obj) {
+    if(obj.hasOwnProperty(key)) {
+      pArray.push([key,obj[key]]);
+    }
   }
   return pArray;
 };
--- cgg-core/src/pt/webdetails/cgg/resources/document.js
+++ cgg-core/src/pt/webdetails/cgg/resources/document.js
@@ -1,92 +1,104 @@
 window = this;
 
-
 var cgg = cgg || {};
-cgg.document = function(_doc){
+
+cgg.document = function(_doc) {
   var doc = {
-    get _node() {return _doc;},
+    get _node() { return _doc; },
+
     get implementation()    { return _doc.getImplementation(); },
-    get svgImplementation() { return 'batik'; },
+    get svgImplementation() { return 'batik'; }, // this feeds pv.renderer
     get documentElement()   { return new cgg.element(_doc.getDocumentElement()); },
-    get lastChild()  { return new cgg.element(_doc.getLastChild());  },
-    get firstChild() { return new cgg.element(_doc.getFirstChild()); },
+    get lastChild()         { return new cgg.element(_doc.getLastChild());  },
+    get firstChild()        { return new cgg.element(_doc.getFirstChild()); },
 
     appendChild: function(e) {
-      /* We need to appendChild() the raw Java element, but
-       * also need to return the wrapped element.
+      /* We need to appendChild() the raw Java element, 
+       * but also need to return the wrapped element.
        * If the element wasn't wrapped to begin with, we must wrap it.
        */
-      if (typeof (_e = e._node) !== "undefined") {
-        _doc.appendChild(_e)
+      var _e = e._node;
+      if(_e) {
+        _doc.appendChild(_e);
         return e;
-      } else {
-        _doc.appendChild(e);
-        return new cgg.element(e);
       }
+
+      _doc.appendChild(e);
+      return new cgg.element(e);
     },
-    getElementById: function(id) {return new cgg.element(_doc.getElementById(id));},
-    createElement: function(tagName) {return new cgg.element(_doc.createElement(tagName));},
-    createTextNode: function(text) {return new cgg.element(_doc.createTextNode(text));},
-    createElementNS: function(nameSpace, qualifiedName) {return cgg.element(_doc.createElementNS(nameSpace, qualifiedName));},
-    removeChild: function(node) {_doc.removeChild(node._node? node._node:node);return node.node? node: new cgg.element(node);}
+    getElementById:  function(id)      { return new cgg.element(_doc.getElementById(id));     },
+    createElement:   function(tagName) { return new cgg.element(_doc.createElement(tagName)); },
+    createTextNode:  function(text)    { return new cgg.element(_doc.createTextNode(text));   },
+    createElementNS: function(nameSpace, qualifiedName) { return cgg.element(_doc.createElementNS(nameSpace, qualifiedName)); },
+    removeChild:     function(node) { 
+      _doc.removeChild(node._node || node);
+      return node.node ? node : new cgg.element(node);
+    }
   };
   return doc;
 };
 
 cgg.element = function(_el) {
   var el = {
-    get _node(){return _el;},
-    get lastChild(){return _el.getLastChild();},
-    get style(){
-        return new cgg.style(_el.getStyle());
-    },
+    get _node() { return _el; },
+
+    get lastChild() { return _el.getLastChild(); },
+    get style()     { return new cgg.style(_el.getStyle()); },
+
     get parentNode() {
       if(_el.getParentNode() == null) { return null };
       return new cgg.element(_el.getParentNode());
     },
+
     addEventListener: function() {},
+
     appendChild: function(e) {
-      /* We need to appendChild() the raw Java element, but
-       * also need to return the wrapped element.
+      /* We need to appendChild() the raw Java element, 
+       * but also need to return the wrapped element.
        * If the element wasn't wrapped to begin with, we must wrap it.
        */
-      if (typeof (_e = e._node) !== "undefined") {
+      var _e = e._node;
+      if(_e) {
         _el.appendChild(_e);
         return e;
-      } else {
-        _el.appendChild(e);
-        return new cgg.element(e);
       }
+
+      _el.appendChild(e);
+      return new cgg.element(e);
     },
-    getElementsByTagName: function (tagName) {return _el.getElementsByTagName(tagName);},    
-    setAttribute: function(attrName, value) {_el.setAttribute(attrName, value);},
-    setAttributeNS: function(ns, attrName, value) {_el.setAttributeNS(ns,attrName, value);},
-    removeAttribute: function(attrName) {_el.removeAttribute(attrName);},
-    removeChild: function(node) {_el.removeChild(node._node? node._node:node);return (node.node? node: new cgg.element(node));},
-    getBBox: function() {return _el.getBBox();}
+    removeChild: function(node) {
+      _el.removeChild(node._node || node);
+      return node.node ? node : new cgg.element(node);
+    },
+
+    getBBox:              function() { return _el.getBBox(); },
+    getElementsByTagName: function(tagName)             { return _el.getElementsByTagName(tagName); },
+    setAttribute:         function(attrName, value)     { _el.setAttribute(attrName, value); },
+    setAttributeNS:       function(ns, attrName, value) { _el.setAttributeNS(ns,attrName, value); },
+    removeAttribute:      function(attrName)            { _el.removeAttribute(attrName); }
   };
   return el;
 };
 
 cgg.style = function(_style) {
   var style = {
-    setProperty: function(name, value,prio) {
+    setProperty: function(name, value, prio) {
       try {
-        _style.setProperty(name, value, (typeof prio === "undefined" ? null :prio));
-      } catch (e) {
-        if (!(e.javaException.getClass().getName() == "org.w3c.dom.DOMException")) { throw e;}
+        _style.setProperty(name, value, (typeof prio === "undefined" ? null : prio));
+      } catch(e) {
+        if(!(e.javaException.getClass().getName() == "org.w3c.dom.DOMException")) { throw e; }
       }
     },
-    removeProperty: function(name) {_style.removeProperty(name);},
-    getProperty: function(name) {
-     return _style.getPropertyValue(name); 
-    }
+    removeProperty: function(name) { _style.removeProperty(name); },
+    getProperty:    function(name) { return _style.getPropertyValue(name); }
   };
   return style;
-}
+};
+
 document = new cgg.document(_document);
 
-window.console = {};
-console.log = function() {
-print("LOG: " + Array.prototype.join.call(arguments,','));
-}
+window.console = {
+  log: function() {
+    print("LOG: " + Array.prototype.join.call(arguments, ','));
+  }
+};
