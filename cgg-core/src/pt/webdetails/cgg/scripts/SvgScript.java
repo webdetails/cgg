29c9783b62f30d1117398922ce4d83885688ea4e
stanislau-strelchanka <stanislau_strelchanka@epam.com>
2016-12-08 16:08:38 +0300

    [PPP-3581] CVE-2015-0250 - Batik 1.7 is vulnerable to XXE in SVG to PNG and SVG to JPG conversion classes

--- cgg-core/src/pt/webdetails/cgg/scripts/SvgScript.java
+++ cgg-core/src/pt/webdetails/cgg/scripts/SvgScript.java
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2002 - 2013 Webdetails, a Pentaho company.  All rights reserved.
+* Copyright 2002 - 2016 Webdetails, a Pentaho company.  All rights reserved.
 * 
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -21,8 +21,8 @@ import org.apache.batik.bridge.DocumentLoader;
 import org.apache.batik.bridge.UserAgent;
 import org.apache.batik.bridge.UserAgentAdapter;
 import org.apache.batik.css.engine.CSSEngine;
-import org.apache.batik.dom.svg.SVGDOMImplementation;
-import org.apache.batik.dom.svg.SVGOMDocument;
+import org.apache.batik.anim.dom.SVGDOMImplementation;
+import org.apache.batik.anim.dom.SVGOMDocument;
 import org.apache.batik.bridge.GVTBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
