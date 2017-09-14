eea71102dac089f775835ce7657f06ce7539099a
Duarte Cunha Leão <duarte.leao@gmail.com>
2017-09-14 18:15:06 +0100

    [BACKLOG-18432] Update due to VizAPI changes. (#105)
    * [BACKLOG-18432] Update due to VizAPI changes.
    
    * [BACKLOG-18432] Update due to VizAPI changes.
    
    * Moved to using `pentaho/instanceInfo` instead of `pentaho/service`.
    Registered karaf/config so that printing can receive OEM configurations placed here.
    
    * [BACKLOG-18432] Fixing AMD configuration

--- cgg-core/src/pt/webdetails/cgg/resources/define-cfg.js
+++ cgg-core/src/pt/webdetails/cgg/resources/define-cfg.js
@@ -17,6 +17,7 @@
 
   var basePathCommonUI = "/plugin/common-ui/resources/web/";
   var basePathCdf = "/plugin/pentaho-cdf/js/";
+  var basePathKarafConfig = "/system/karaf/config/web-client/";
 
   require.config({
     loadSync: loadSync,
@@ -24,8 +25,7 @@
     packages: [
       "cgg",
       "cdf",
-      "ccc",
-      {"name": "pentaho/visual/base", "main": "model"}
+      "ccc"
     ],
 
     // module -> path
@@ -34,8 +34,10 @@
       "json": basePathCommonUI + "util/require-json/json",
       "text": basePathCommonUI + "util/require-text/text",
       "pentaho": basePathCommonUI + "pentaho",
+      "cdf/_context": basePathCdf + "_context",
       "cdf/PentahoTypeContext": basePathCdf + "PentahoTypeContext",
-      "cdf/components/ccc/config/cdf.vizApi.conf": basePathCdf + "components/ccc/config/cdf.vizApi.conf"
+      "cdf/components/ccc/config/cdf.vizApi.conf": basePathCdf + "components/ccc/config/cdf.vizApi.conf",
+      "pentaho/config/deploy": basePathKarafConfig
     },
 
     // module -> module
@@ -65,10 +67,97 @@
         useGlobal: true
       },
 
-      "pentaho/service": {
-        "pentaho/visual/config/vizApi.conf": "pentaho.config.spec.IRuleSet",
-        "cdf/components/ccc/config/cdf.vizApi.conf": "pentaho.config.spec.IRuleSet",
-        "pentaho/config/impl/instanceOfAmdLoadedService": "pentaho.config.IService"
+      // ATTENTION: this AMD information is duplicated from parts of common-ui and cdf.
+      // Keep all in sync.
+
+      "pentaho/instanceInfo": {
+        "pentaho/visual/config/vizApi.conf": {type: "pentaho.config.spec.IRuleSet"},
+        "cdf/components/ccc/config/cdf.vizApi.conf": {type: "pentaho.config.spec.IRuleSet"},
+        "pentaho/config/deploy/config": {type: "pentaho.config.spec.IRuleSet"},
+        "pentaho/config/impl/instanceOfAmdLoadedService": {type: "pentaho.config.IService"},
+
+        "pentaho/visual/color/palettes/nominalPrimary": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/nominalNeutral": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/nominalLight": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/nominalDark": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/quantitativeBlue3": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/quantitativeBlue5": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/quantitativeGray3": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/quantitativeGray5": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/divergentRyg3": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/divergentRyg5": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/divergentRyb3": {type: "pentaho/visual/color/palette"},
+        "pentaho/visual/color/palettes/divergentRyb5": {type: "pentaho/visual/color/palette"}
+      },
+
+      "pentaho/typeInfo": {
+        "pentaho/type/instance": {alias: "instance"},
+        "pentaho/type/value": {alias: "value", base: "instance"},
+        "pentaho/type/property": {alias: "property", base: "instance"},
+        "pentaho/type/list": {alias: "list", base: "value"},
+        "pentaho/type/element": {alias: "element", base: "value"},
+        "pentaho/type/complex": {alias: "complex", base: "element"},
+        "pentaho/type/application": {alias: "application", base: "complex"},
+        "pentaho/type/model": {alias: "model", base: "complex"},
+        "pentaho/type/simple": {alias: "simple", base: "element"},
+        "pentaho/type/number": {alias: "number", base: "simple"},
+        "pentaho/type/string": {alias: "string", base: "simple"},
+        "pentaho/type/boolean": {alias: "boolean", base: "simple"},
+        "pentaho/type/date": {alias: "date", base: "simple"},
+        "pentaho/type/object": {alias: "object", base: "simple"},
+        "pentaho/type/function": {alias: "function", base: "simple"},
+        "pentaho/type/mixins/enum": {alias: "enum", base: "element"},
+        "pentaho/type/action/base": {base: "element"},
+
+        "pentaho/data/filter/abstract": {base: "complex"},
+        "pentaho/data/filter/true": {alias: "true", base: "pentaho/data/filter/abstract"},
+        "pentaho/data/filter/false": {alias: "false", base: "pentaho/data/filter/abstract"},
+        "pentaho/data/filter/tree": {base: "pentaho/data/filter/abstract"},
+        "pentaho/data/filter/or": {alias: "or", base: "pentaho/data/filter/tree"},
+        "pentaho/data/filter/and": {alias: "and", base: "pentaho/data/filter/tree"},
+        "pentaho/data/filter/not": {alias: "not", base: "pentaho/data/filter/abstract"},
+        "pentaho/data/filter/property": {base: "pentaho/data/filter/abstract"},
+        "pentaho/data/filter/isEqual": {alias: "=", base: "pentaho/data/filter/property"},
+        "pentaho/data/filter/isIn": {alias: "in", base: "pentaho/data/filter/property"},
+        "pentaho/data/filter/isGreater": {alias: ">", base: "pentaho/data/filter/property"},
+        "pentaho/data/filter/isGreaterOrEqual": {alias: ">=", base: "pentaho/data/filter/property"},
+        "pentaho/data/filter/isLess": {alias: "<", base: "pentaho/data/filter/property"},
+        "pentaho/data/filter/isLessOrEqual": {alias: "<=", base: "pentaho/data/filter/property"},
+        "pentaho/data/filter/isLike": {alias: "like", base: "pentaho/data/filter/property"},
+
+        "pentaho/visual/base/model": {base: "model"},
+        "pentaho/visual/base/view": { base: "complex"},
+
+        "pentaho/visual/models/abstract": {base: "pentaho/visual/base/model"},
+        "pentaho/visual/samples/calc/model": {base: "pentaho/visual/base/model"},
+        "pentaho/visual/models/cartesianAbstract": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/categoricalContinuousAbstract": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barAbstract": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barNormalizedAbstract": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barHorizontal": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/bar": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barStacked": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barStackedHorizontal": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barNormalized": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barNormalizedHorizontal": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/barLine": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/line": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/pointAbstract": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/metricPointAbstract": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/areaStacked": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/pie": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/heatGrid": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/sunburst": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/donut": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/scatter": {base: "pentaho/visual/models/abstract"},
+        "pentaho/visual/models/bubble": {base: "pentaho/visual/models/abstract"},
+
+        "pentaho/visual/action/base": {base: "pentaho/type/action/base"},
+        "pentaho/visual/action/data": {base: "pentaho/visual/action/base"},
+        "pentaho/visual/action/select": {alias: "select", base: "pentaho/visual/action/data"},
+        "pentaho/visual/action/execute": {alias: "execute", base: "pentaho/visual/action/data"},
+
+        "pentaho/visual/color/palette": {base: "complex"}
       },
 
       "pentaho/environment": {
