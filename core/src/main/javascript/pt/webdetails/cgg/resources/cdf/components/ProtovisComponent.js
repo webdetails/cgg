8e5b160dfa5decee4e80f5a7f8ea8a655a8825ea
Duarte Cunha Leão <duarte.leao@gmail.com>
2021-05-27 16:24:56 +0100

    [SP-6087] Backport of ANALYZER-3942 - Add Intl API support to CGG's JavaScript environment and fix localized string resources (#148)
    * WebResourceLoader to pass Pentaho session's locale override via trusted proxy arguments
      - this allows locale sensitive resources, such as /i18n, to be more faithfully proxied.
    * Add Intl API support to CGG's JavaScript environment
      - Upgraded Rhino to version 1.7.13 which already supports many ES6 features.
      - Remove JS shims which are no longer needed in the new Rhino version
      - Added the js-intl-polyfill module which creates the Intl polyfill used by CGG
      - Removed "useGlobal" flag and corresponding functionality from the CGG JS environment (was never used); cgg is always installed on the global scope.
    * Change NodeJS version used to build the js-intl-polyfill module to the latest LTS

--- core/src/main/javascript/pt/webdetails/cgg/resources/cdf/components/ProtovisComponent.js
+++ core/src/main/javascript/pt/webdetails/cgg/resources/cdf/components/ProtovisComponent.js
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2002 - 2017 Webdetails, a Hitachi Vantara company.  All rights reserved.
+* Copyright 2002 - 2021 Webdetails, a Hitachi Vantara company.  All rights reserved.
 *
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -13,10 +13,9 @@
 define([
     'require',
     'cgg',
-    'util',
     '../dash',
     './ccc/SvgComponent'
-], function(require, cgg, util, dash, SvgComponent) {
+], function(require, cgg, dash, SvgComponent) {
 
     var ProtovisComponent = SvgComponent.extend({
         vis: null,
@@ -26,9 +25,7 @@ define([
             this.base();
 
             this.pv = require('ccc!protovis-standalone');
-            if(cgg.useGlobal) {
-                util.global.pv = this.pv;
-            }
+            globalThis.pv = this.pv;
         },
 
         renderCore: function(cdaData) {
--- core/src/main/javascript/pt/webdetails/cgg/resources/define-cfg.js
+++ core/src/main/javascript/pt/webdetails/cgg/resources/define-cfg.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2019 Webdetails, a Hitachi Vantara company.  All rights reserved.
+ * Copyright 2002 - 2021 Webdetails, a Hitachi Vantara company.  All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -64,10 +64,6 @@
     },
 
     config: {
-      "cgg/main": {
-        useGlobal: true
-      },
-
       // ATTENTION: this AMD information is duplicated from parts of common-ui and cdf.
       // Keep all in sync.
 
@@ -444,6 +440,8 @@
       },
 
       "pentaho/environment": {
+        // convert to actual JS string
+        locale: ("" + params.get("LOCALE")),
         server: {
           // requirejs does not like to mixin java objects
           // convert to JS string
