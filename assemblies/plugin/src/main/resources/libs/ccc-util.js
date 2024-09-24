d1642d3a252bb415f259ab27f0c877dc6ee2d08a
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:11:09 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit edefe91e4278afaa9c49400e1ed777da89e6945a.

--- assemblies/plugin/src/main/resources/libs/ccc-util.js
+++ assemblies/plugin/src/main/resources/libs/ccc-util.js
@@ -1,13 +1,2 @@
-/*! ******************************************************************************
- *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
 // required by analyzer 4.8 and 5.0 print scripts.
 // ToDo: when can we remove this?
