bf65e562a5c1f70e5a3aed390203aedbc8e86613
SahithiKommagani6 <sahithi.kommagani@hitachivantara.com>
2024-10-22 17:36:06 +0530

    [BACKLOG-41287]-Upgrading cgg from Java EE to Jakarta EE to support with Tomcat-10.X

--- core/src/main/java/pt/webdetails/cgg/SVGChart.java
+++ core/src/main/java/pt/webdetails/cgg/SVGChart.java
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2018 Webdetails, a Hitachi Vantara company.  All rights reserved.
+ * Copyright 2002 - 2024 Webdetails, a Hitachi Vantara company.  All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -30,11 +30,11 @@ import pt.webdetails.cgg.output.PngOutputHandler;
 import pt.webdetails.cgg.output.SVGOutputHandler;
 import pt.webdetails.cpf.utils.CharsetHelper;
 
-import javax.activation.DataHandler;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.util.ByteArrayDataSource;
+import jakarta.activation.DataHandler;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMultipart;
+import jakarta.mail.util.ByteArrayDataSource;
 
 /**
  * @author pdpi
--- pentaho/src/main/java/pt/webdetails/cgg/WebCgg.java
+++ pentaho/src/main/java/pt/webdetails/cgg/WebCgg.java
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2002 - 2017 Webdetails, a Hitachi Vantara company.  All rights reserved.
+* Copyright 2002 - 2024 Webdetails, a Hitachi Vantara company.  All rights reserved.
 *
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -15,7 +15,7 @@ package pt.webdetails.cgg;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 import pt.webdetails.cgg.datasources.WebDataSourceFactory;
 import pt.webdetails.cgg.output.OutputHandler;
