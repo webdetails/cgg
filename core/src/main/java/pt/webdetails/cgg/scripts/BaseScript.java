1dc26e44845692c1f2905c497edf8e5db4524482
Alan Bryant <alan.bryant@hitachivantara.com>
2026-08-07 15:53:53 -0400

    [PPP-5970] - upgrade to commons-lang3

--- core/src/main/java/pt/webdetails/cgg/scripts/BaseScript.java
+++ core/src/main/java/pt/webdetails/cgg/scripts/BaseScript.java
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mozilla.javascript.Context;
